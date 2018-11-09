655da8c103c8b7bc8805a71f8dfaa883ed1b53fd
dylancromer <dylan@dylancromer.com>
2018-11-08 21:11:20 -0500

    Fixed a couple things

--- startup.sh
+++ startup.sh
@@ -2,9 +2,12 @@
 set -e
 
 #Set these yourself
+
 SITE_REPOSITORY_NAME="<NAME>"
 USERNAME="<USERNAME>"
-#Uncomment and set if you want to add a theme automatically
+
+#Uncomment and set if you want to add a theme automatically (Must also uncomment the git submodule add command)
+
 #THEME_USERNAME="<THEMEUSERNAME>"
 #THEME_NAME="<THEMENAME>"
 
@@ -12,7 +15,6 @@ USERNAME="<USERNAME>"
 SITE_REPOSITORY="https://github.com/$USERNAME/$SITE_REPOSITORY_NAME"
 DEV_REPOSITORY="https://github.com/$USERNAME/$SITE_REPOSITORY_NAME-dev"
 
-#Sets up directories
 cd ..
 mkdir hugo_sites && cd hugo_sites
 
@@ -26,6 +28,7 @@ git init && git add remote origin $DEV_REPOSITORY
 git submodule add -b master git@github.com:$USERNAME/$USERNAME.github.io.git public
 
 #Uncomment if you want to add a theme automatically
-#git submodule add https://github.com/$THEME_USERNAME/$THEME_NAME 
+
+#git submodule add https://github.com/$THEME_USERNAME/$THEME_NAME themes/$THEME_NAME
 
 cd ../hugolaunch
