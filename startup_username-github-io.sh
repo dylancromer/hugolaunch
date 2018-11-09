1591aa34a7e65097d16fee88ad74b67a46454b18
dylancromer <dylan@dylancromer.com>
2018-11-08 21:34:50 -0500

    Expanded scripts and fixed another issue

--- startup_username-github-io.sh
+++ startup_username-github-io.sh
@@ -3,17 +3,16 @@ set -e
 
 #Set these yourself
 
-SITE_REPOSITORY_NAME="hugotest"
-USERNAME="dylancromer"
+USERNAME="<USERNAME>"
 
 #Uncomment and set if you want to add a theme automatically (Must also uncomment the git submodule add command)
 
-THEME_USERNAME="vickylai"
-THEME_NAME="hugo-theme-sam"
+#THEME_USERNAME="<THEMEUSERNAME>"
+#THEME_NAME="<THEMENAME>"
 
 #Do not change unless you use some other git service
-SITE_REPOSITORY="https://github.com/$USERNAME/$SITE_REPOSITORY_NAME"
-DEV_REPOSITORY="https://github.com/$USERNAME/$SITE_REPOSITORY_NAME-dev"
+SITE_REPOSITORY="https://github.com/$USERNAME/$USERNAME.github.io"
+DEV_REPOSITORY="https://github.com/$USERNAME/$USERNAME.github.io-dev"
 
 cd ..
 
@@ -30,8 +29,8 @@ cp ../hugolaunch/deploy.sh $SITE_REPOSITORY_NAME
 cd $SITE_REPOSITORY_NAME
 git init && git remote add origin $DEV_REPOSITORY
 
-git submodule add -b master git@github.com:$USERNAME/$SITE_REPOSITORY_NAME public
+git submodule add -b master git@github.com:$USERNAME/$USERNAME.github.io.git public
 
 #Uncomment if you want to add a theme automatically
 
-git submodule add https://github.com/$THEME_USERNAME/$THEME_NAME themes/$THEME_NAME
+#git submodule add https://github.com/$THEME_USERNAME/$THEME_NAME themes/$THEME_NAME
